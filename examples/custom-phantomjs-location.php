190a113775ce22560b3e0433cb3002b6248f77f8
Jonny Wenmoth <services@jonnyw.me>
2015-12-17 11:49:26 +0000

    Removing outdated docs and examples

--- examples/custom-phantomjs-location.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require '../vendor/autoload.php';
-
-use JonnyW\PhantomJs\Client;
-
-$client = Client::getInstance();
-$client->setPhantomJs('/path/to/phantomjs'); // PhantomJS executbale file path
