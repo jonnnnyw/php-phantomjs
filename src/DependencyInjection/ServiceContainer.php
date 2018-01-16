03f4df86ee109fb091e7b1c939bab54df88f644b
Denis K <dk.pochtamp@gmail.com>
2018-01-16 13:59:30 +0800

    Update ServiceContainer.php
    Relevant issue in parent repo: 
    
    #216
    Declaration of JonnyW\..\ServiceContainer::load() should be compatible with Symfony\..\Container::load($file)

--- src/DependencyInjection/ServiceContainer.php
+++ src/DependencyInjection/ServiceContainer.php
@@ -45,7 +45,7 @@ class ServiceContainer extends ContainerBuilder
     /**
      * Load service container.
      */
-    public function load()
+    public function load($file)
     {
         $loader = new YamlFileLoader($this, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('config.yml');
